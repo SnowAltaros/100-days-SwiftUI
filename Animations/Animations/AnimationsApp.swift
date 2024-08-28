8683a8e02c8859a842b1ef2c2b4b827926b5525e
Stanislav Popovci <p.stas94@icloud.com>
2024-08-28 19:27:39 +0100

    Add files via upload

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Stanislav Popovici on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
